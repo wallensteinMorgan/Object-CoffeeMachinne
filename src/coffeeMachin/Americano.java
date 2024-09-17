758a59f262b79455a13bcd73adbc184ede9de017
wallensteinMorgan <mrs.morrigan@mail.ru>
2024-09-17 14:42:35 +0300

    new

--- src/coffeeMachin/Americano.java
+++ src/coffeeMachin/Americano.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Americano implements Coffee {
     @Override
     public void makeCoffee(String name) {
-        System.out.println("Вы выбрали американо " + name);
+        System.out.println("Вы выбрали американо" + name);
         Scanner cons = new Scanner(System.in);
         if ((Ingredients.water < 4) || (Ingredients.coffeeBeans < 9) || (Ingredients.milk < 19)) {
             Menu.noIngredients();
